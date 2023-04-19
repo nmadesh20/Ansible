37b5a6d7b2f5564544ef7c29449c9fdf6eadf3c4
madeshnagaraj <nmadesh20@gmail.com>
2023-04-19 21:02:03 +0530

    Initial Commit

---
+++ nodejs/app/app.js
@@ -0,0 +1,13 @@
+// Load the express module.
+var express = require('express'),
+    app = express.createServer();
+
+// Respond to requests for / with 'Hello World'.
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+    res.send('From Ansible')
+});
+
+// Listen on port 80 (like a true web server).
+app.listen(80);
+console.log('Express server started successfully.');
